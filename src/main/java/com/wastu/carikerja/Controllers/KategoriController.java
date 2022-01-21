0aae839039fd8c0f3cd0c85a9803912c78594355
R. Bagas <87528336+10120128@users.noreply.github.com>
2022-01-21 10:46:01 +0700

    Menambahkan model dan controller kategori

---
+++ src/main/java/com/wastu/carikerja/Controllers/KategoriController.java
@@ -0,0 +1,65 @@
+package com.wastu.carikerja.Controllers;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import com.wastu.carikerja.Helpers.DatabaseHelper;
+import com.wastu.carikerja.Models.Kategori;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class KategoriController {
+    private final Dao<Kategori, Long> kategoriDao;
+    private static KategoriController instance;
+
+    public static synchronized KategoriController getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new KategoriController();
+        }
+        return instance;
+    }
+
+    private KategoriController() throws SQLException {
+        ConnectionSource connectionSource = DatabaseHelper.getInstance().getConnectionSource();
+        this.kategoriDao = DaoManager.createDao(connectionSource, Kategori.class);
+        TableUtils.createTableIfNotExists(connectionSource, Kategori.class);
+    }
+
+    public Kategori get(long id) throws Exception {
+        Kategori kategori = kategoriDao.queryForId(id);
+        if (kategori == null) {
+            throw new Exception("Kategori tidak ditemukan");
+        }
+        return kategori;
+    }
+
+    public List<Kategori> list() throws SQLException {
+        return kategoriDao.queryForAll();
+    }
+
+    public void update(long id, String nama) throws SQLException {
+        Kategori kategori = kategoriDao.queryForId(id);
+        kategori.setNama(nama);
+        kategoriDao.update(kategori);
+    }
+
+    public void create(Kategori kategori) throws Exception {
+
+        // Jika ada maka throw exception
+        if (kategoriDao.queryForEq("nama", kategori.getNama()).stream().findFirst().orElse(null) != null) {
+            throw new Exception("Kategori sudah ada");
+        }
+
+        kategoriDao.create(kategori);
+    }
+
+    public void delete(long id) throws SQLException {
+        kategoriDao.deleteById(id);
+    }
+
+    public boolean isExists(long id) throws SQLException {
+        return kategoriDao.queryForId(id) != null;
+    }
+}
---
+++ src/main/java/com/wastu/carikerja/Models/Kategori.java
@@ -0,0 +1,42 @@
+package com.wastu.carikerja.Models;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+import com.wastu.carikerja.Enums.UserRole;
+
+@DatabaseTable(tableName = "kategori")
+public class Kategori {
+    @DatabaseField(generatedId = true)
+    private long id;
+    @DatabaseField(unique = true)
+    private String nama;
+
+    public Kategori() {
+    }
+
+    public Kategori(long id, String nama) {
+        this.id = id;
+        this.nama = nama;
+    }
+
+    public Kategori(String nama) {
+        this.nama = nama;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
