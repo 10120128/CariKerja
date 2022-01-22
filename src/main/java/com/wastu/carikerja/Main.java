8e63a45d31e046ef9c469b3f166e4960f62defd7
R. Bagas <87528336+10120128@users.noreply.github.com>
2022-01-22 08:30:38 +0700

    Lazy loading di awal

--- src/main/java/com/wastu/carikerja/Main.java
+++ src/main/java/com/wastu/carikerja/Main.java
@@ -1,7 +1,10 @@
 package com.wastu.carikerja;
 
+import com.wastu.carikerja.Controllers.KategoriController;
+import com.wastu.carikerja.Controllers.UserController;
 import com.wastu.carikerja.Helpers.DatabaseHelper;
 import com.wastu.carikerja.Views.MainMenuView;
+import com.wastu.carikerja.Views.Menu.UserMenuView;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
 
@@ -13,6 +16,15 @@ public class Main {
         //TODO : Semua menu dan fitur bisa kembali
         try {
             TextIO mainTextIO = TextIoFactory.getTextIO();
+
+            // Lazy loading
+            mainTextIO.getTextTerminal().setBookmark("main");
+            mainTextIO.getTextTerminal().println("Mohon tunggu sebentar....");
+            DatabaseHelper.getInstance();
+            KategoriController.getInstance();
+            UserController.getInstance();
+            UserController.getInstance();
+            mainTextIO.getTextTerminal().resetToBookmark("main");
 
             MainMenuView cariKerja = MainMenuView.getInstance();
             cariKerja.show();
